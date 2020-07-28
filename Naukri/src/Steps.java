e812e1e0c8fe6f3fc45857f62d43aa4c3901fe07
divyag886 <divya.ganta886@gmail.com>
2020-07-28 10:09:34 +0100

    TC_003 Added
    TC_003 Added Succesfully

---
+++ Naukri/src/Steps.java
@@ -0,0 +1,8 @@
+
+public class Steps {
+	
+	//Step 1 : Right click on Default Package Create Class and name it as "TC_002"
+	//Step 2 : While creating class click pubic static void (checkbox)
+	//Step 3 : Copy Print Line from "TC_001" and Paste it in "TC_002"
+	//Step 4 : Rename to "scenario 2 : To verify password field" 
+}
---
+++ Naukri/src/TC_003.java
@@ -0,0 +1,16 @@
+
+public class TC_003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("---------------------------------------------------------------");
+		System.out.println("Test Started");
+		System.out.println("---------------------------------------------------------------");
+		System.out.println("Expected Result : Email Id Error Message should be in red color");
+		System.out.println("Actual Result : Working as expected");
+		System.out.println("---------------------------------------------------------------");
+		System.out.println("Test Ended");
+		System.out.println("---------------------------------------------------------------");
+	}
+
+}

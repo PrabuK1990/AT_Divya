81f5831a90833f2ee4664e66cdada8c32f9407a7
divyag886 <divya.ganta886@gmail.com>
2020-07-28 18:27:11 +0100

    Create TC_005.java
    for practice created login class

---
+++ Naukri/src/TC_005.java
@@ -0,0 +1,16 @@
+
+public class TC_005 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+login();
+
+	}
+
+	private static void login() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter User Name");
+		System.out.println("Enter password");
+	}
+
+}

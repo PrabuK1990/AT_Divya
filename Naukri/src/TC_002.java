5cc8312b547a211f86cdb323c6047043385d2210
divyag886 <divya.ganta886@gmail.com>
2020-07-29 11:56:32 +0100

    TC_003 Developed
    open facebook webpage using with Chrome

--- Naukri/src/TC_002.java
+++ Naukri/src/TC_002.java
@@ -8,19 +8,20 @@ public class TC_002 {
 		System.out.println("Test Starts");
 
 		// Set path to locate firefox exe file
-		System.setProperty("webdriver.gecko.driver", "");
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\user\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
 
 		// Create instance of FirefoxDriver() class
 		// Store it in object and name it as "firefox"
 		// Declare the interface type before as "WebDriver"
-		
+		WebDriver firefox = new GeckoDriver();
+		//WebDriver firefox= new ChromeDriver();
 
 		// Enter URL in the browser
-		
+		firefox.get("https://www.naukri.com/");
 		// Close current browser
-
+		firefox.close();
 		// Close all browsers
-		
+		firefox.quit();
 
 		System.out.println("Test Ends");
 
--- Naukri/src/TC_003.java
+++ Naukri/src/TC_003.java
@@ -1,8 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TC_003 {
 
 	public static void main(String[] args) {
 		
 		//Launch Chrome Browser and enter Facebook URL
+System.out.println("Test Starts");
+		
+		//Set path to locate chromedriver exe file
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		
+		//Create instance of ChromeDriver() class
+		//Store it in object and name it as "chrome"
+		//Declare the interface type before as "WebDriver"
+		WebDriver chrome = new ChromeDriver();
+		
+		//Enter URL in the browser
+		chrome.get("https://www.facebook.com/");
+		
+		//Close current browser
+		//chrome.close();
+		
+		//Close all browsers
+		chrome.quit();
+		
+		System.out.println("Test Ends");
 	}
 }

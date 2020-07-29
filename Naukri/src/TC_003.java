b94c7af6f161b7ea6499faff5399e4cc1acfb112
divyag886 <divya.ganta886@gmail.com>
2020-07-29 11:21:58 +0100

    Update TC_003.java

--- Naukri/src/TC_003.java
+++ Naukri/src/TC_003.java
@@ -3,13 +3,6 @@ public class TC_003 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("---------------------------------------------------------------");
-		System.out.println("Test Started");
-		System.out.println("---------------------------------------------------------------");
-		System.out.println("Expected Result : Email Id Error Message should be in red color");
-		System.out.println("Actual Result : Working as expected");
-		System.out.println("---------------------------------------------------------------");
-		System.out.println("Test Ended");
-		System.out.println("---------------------------------------------------------------");
+		//Launch Chrome Browser and enter Facebook URL
 	}
 }

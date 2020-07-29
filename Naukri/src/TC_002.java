e59ec3620c8f715742f23bddb5fb2e887fae3dc1
divyag886 <divya.ganta886@gmail.com>
2020-07-29 13:42:24 +0100

    Firefox Browser setup added
    Created function to launch Firefox. Given the url  as Naukri.com. exicuted succesfully.

--- Naukri/src/TC_002.java
+++ Naukri/src/TC_002.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TC_002 {
 
@@ -9,18 +10,19 @@ public class TC_002 {
 
 		// Set path to locate firefox exe file
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\user\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
-
 		// Create instance of FirefoxDriver() class
 		// Store it in object and name it as "firefox"
 		// Declare the interface type before as "WebDriver"
-		WebDriver firefox = new GeckoDriver();
-		//WebDriver firefox= new ChromeDriver();
-
+		
+		WebDriver firefox = new FirefoxDriver();
 		// Enter URL in the browser
+		
 		firefox.get("https://www.naukri.com/");
 		// Close current browser
+		
 		firefox.close();
 		// Close all browsers
+		
 		firefox.quit();
 
 		System.out.println("Test Ends");

acdfeae1ecbd5cee5e5ab8d36c2fa5d121e99f7d
divyag886 <divya.ganta886@gmail.com>
2020-08-04 17:08:32 +0100

    Added Inputs Using with Interface
    Added Inputs for Register WebPage and Employe WebPage in the Interface Folder NaukriControls

--- Naukri/src/NaukriInputs.java
+++ Naukri/src/NaukriInputs.java
@@ -12,5 +12,54 @@ public interface NaukriInputs {
 	public static String NaukriInput_PassWord1 = "pass123";
 	public static String NaukriInput_PassWord2 = "Divya123";
 	public static String NaukriInput_INPassWord = "123##";
+	public static String NaukriInput_EmployeName1 = "Srivani";
+	public static String NaukriInput_EmployeName2 = "Srivani123";
+	public static String NaukriInput_EmployeName3 = "SriV";
+	public static String NaukriInput_EmployeINName1 = "XvX@";
+	public static String NaukriInput_EmployeINName2 = "@@34";
+	public static int NaukriInput_EmployeContactNumber1 = 1234567890;
+	public static int NaukriInput_EmployeContactNumber2 = 1237897890;
+	public static int NaukriInput_EmployeContactINNumber1 = 1111;
+	public static int NaukriInput_EmployeContactINNumber2 = 12300;
+	public static String NaukriInput_EmployeCompanyName1 = "Vedha Ltd Com";
+	public static String NaukriInput_EmployeCompanyName2 = "Elephant.Co.Ltd";
+	public static String NaukriInput_EmployeCompanyINName1 = "*!*&&";
+	public static String NaukriInput_EmployeCompanyINName2 = "{(@)}";
+	public static String NaukriInput_EmployeEmailID1 = "Srivani@gmail.com";	
+	public static String NaukriInput_EmployeEmailID2 = "Srivani888@gmail.com";
+	public static String NaukriInput_EmployeINEmailID1 = "Srivani";
+	public static String NaukriInput_EmployeINEmailID2 = "rivani888mail.com";
+	public static String NaukriInput_EmployeCity1 = "Vishakapatnam";
+	public static String NaukriInput_EmployeCity2 = "Hyderabad";
+	public static String NaukriInput_EmployeINCity1 = "Kalpana";
+	public static String NaukriInput_EmployeINCity2 = "Devirex";
+	public static String NaukriInput_RegisterName1 = "Sujatha";
+	public static String NaukriInput_RegisterName2 = "Jyothi";
+	public static String NaukriInput_RegisterINName1 = "12j";
+	public static String NaukriInput_RegisterINName2 = "589llo";
+	public static String NaukriInput_RegisterEmailID1 = "Sujatha@gmail.com";
+	public static String NaukriInput_RegisterEmailID2 = "Jyothi123@gmail.com";
+	public static String NaukriInput_RegisterINEmailID1 = "Sujatha@@@";
+	public static String NaukriInput_RegisterINEmailID2 = "Jyothi@@@123";
+	public static String NaukriInput_RegisterPassWord1 = "Pass123";
+	public static String NaukriInput_RegisterPassWord2 = "Yes12345";
+	public static String NaukriInput_RegisterINPassWord1 = "Fail123";
+	public static String NaukriInput_RegisterINPassWord2 = "No12345";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }

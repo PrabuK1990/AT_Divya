3cf872ca2dac783374361da3b141df6549e05fd7
divyag886 <divya.ganta886@gmail.com>
2020-08-04 23:29:03 +0100

    Added interface InputsFor Empluyer
    added Inputs for For Employer WebPage with interface given name as NaukriEmployerInputs

---
+++ Naukri/src/NaukriEmployerInputs.java
@@ -0,0 +1,25 @@
+
+public interface NaukriEmployerInputs 
+{
+	public static String NaukriEmployerInputs_EmployeName1 = "Srivani";
+	public static String NaukriEmployerInputs_EmployeName2 = "Srivani123";
+	public static String NaukriEmployerInputs_EmployeName3 = "SriV";
+	public static String NaukriEmployerInputs_EmployeINName1 = "XvX@";
+	public static String NaukriEmployerInputs_EmployeINName2 = "@@34";
+	public static int NaukriEmployerInputs_EmployeContactNumber1 = 1234567890;
+	public static int NaukriEmployerInputs_EmployeContactNumber2 = 1237897890;
+	public static int NaukriEmployerInputs_EmployeContactINNumber1 = 1111;
+	public static int NaukriEmployerInputs_EmployeContactINNumber2 = 12300;
+	public static String NaukriEmployerInputs_EmployeCompanyName1 = "Vedha Ltd Com";
+	public static String NaukriEmployerInputs_EmployeCompanyName2 = "Elephant.Co.Ltd";
+	public static String NaukriEmployerInputs_EmployeCompanyINName1 = "*!*&&";
+	public static String NaukriEmployerInputs_EmployeCompanyINName2 = "{(@)}";
+	public static String NaukriEmployerInputs_EmployeEmailID1 = "Srivani@gmail.com";	
+	public static String NaukriEmployerInputs_EmployeEmailID2 = "Srivani888@gmail.com";
+	public static String NaukriEmployerInputs_EmployeINEmailID1 = "Srivani";
+	public static String NaukriEmployerInputs_EmployeINEmailID2 = "rivani888mail.com";
+	public static String NaukriEmployerInputs_EmployeCity1 = "Vishakapatnam";
+	public static String NaukriEmployerInputs_EmployeCity2 = "Hyderabad";
+	public static String NaukriEmployerInputs_EmployeINCity1 = "Kalpana";
+	public static String NaukriEmployerInputs_EmployeINCity2 = "Devirex";
+}

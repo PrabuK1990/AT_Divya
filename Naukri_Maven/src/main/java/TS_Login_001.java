9158d2c9c3a1184c2064849941572d5a8c8c22d4
divyag886 <divya.ganta886@gmail.com>
2020-08-09 16:07:34 +0100

    Added Test cases InTestScript_001
    Added All Test Case for Login UserName And PassWord Field to TestScript TS_Login_001

--- Naukri_Maven/src/main/java/TS_Login_001.java
+++ Naukri_Maven/src/main/java/TS_Login_001.java
@@ -28,9 +28,9 @@ public class TS_Login_001 implements NaukriInputs {
 		TS_Login_001.waitMethod();
 	}
 
-	// Actual Test Case 01
+	// Actual Test Case 001
 	@Test
-	public void testcase_01() {
+	public void testcase_001() {
 		System.out.println("Check whether the username field accepts login valid inputs login");
 		String usr[] = { NaukriInputs_UserName1, NaukriInputs_UserName2, NaukriInputs_UserName3,
 				NaukriInputs_UserName4 };
@@ -45,9 +45,9 @@ public class TS_Login_001 implements NaukriInputs {
 		System.out.println("No error message should display.");
 	}
 
-	// Actual Test Case 02
+	// Actual Test Case 002
 	@Test
-	public void testcase_02() {
+	public void testcase_002() {
 		System.out.println("Check whether the username field accepts Login Invalid inputs Login");
 
 		String usr[] = { NaukriInputs_InUserName1, NaukriInputs_InUserName2, NaukriInputs_InUserName3 };
@@ -64,9 +64,9 @@ public class TS_Login_001 implements NaukriInputs {
 		System.out.println("Error message should display as Please Enter Valid Email/Password");
 	}
 
-	// Actual Test Case 03
+	// Actual Test Case 003
 	@Test
-	public void testcase_03() {
+	public void testcase_003() {
 		System.out.println("Check whether the textbox outline is in red color");
 
 		String usr[] = { NaukriInputs_InUserName1, NaukriInputs_InUserName2, NaukriInputs_InUserName3 };
@@ -84,8 +84,122 @@ public class TS_Login_001 implements NaukriInputs {
 
 		System.out.println("Email ID Textbox outline should be in red colour");
 	}
+
+	// Actual Test case 004
+	@Test
+	public void testcase_004() {
+		System.out.println("Check whether error message text is in red color");
+
+		String usr[] = { NaukriInputs_InUserName1, NaukriInputs_InUserName2, NaukriInputs_InUserName3 };
+
+		for (int i = 0; i < usr.length; i++) {
+			// Enter username
+			TS_Login_001.enterUsername(usr[i]);// Passing Parameter/Arguement
+			System.out.println(usr[i]);
+			TS_Login_001.waitMethod();
+			TS_Login_001.clickLoginButton();
+			TS_Login_001.clearUsername();
+		}
+
+		System.out.println("Error Message should be in Red Colour");
+
+	}
+
+	// Actual Test case 005
+	@Test
+	public void testcase_005() {
+		System.out.println("Check whether the default included in the email text box");
+		System.out.println("Default Text will be display in Email Id Text Box");
+	}
+
+	// Actual Test case 006
+	@Test
+	public void testcase_006() {
+		System.out.println("Check whether the password field accepts Login valid inputs Login");
+
+		// Enter username
+		TS_Login_001.enterUsername(NaukriInputs_UserName1);
+
+		String usr[] = { NaukriInput_PassWord1, NaukriInput_PassWord2 };
+
+		for (int i = 0; i < usr.length; i++) {
+			TS_Login_001.enterPassword(usr[i]);// Passing Parameter/Arguement
+			System.out.println(usr[i]);
+			TS_Login_001.waitMethod();
+			TS_Login_001.clickLoginButton();
+			TS_Login_001.clearPassword();
+		}
+
+		System.out.println("Open to the web page/Home page");
+	}
 	
+	//Actual Test case 007
+	@Test
+	public void testcase_007()
+	{
+		System.out.println("Check whether the password field accepts Login Invalid inputs Login");
+		TS_Login_001.enterUsername(NaukriInputs_UserName1);
+		
+		//Enter  Invalid Password
+		TS_Login_001.enterPassword(NaukriInput_INPassWord);
+
+		//wait
+		TS_Login_001.waitMethod();
+		
+		//Click on Login Button
+		TS_Login_001.clickLoginButton();
+		
+		System.out.println("Error message should be display as Invalid details, plese check Email ID-password");
+	}
 	
+	//Actual Test Case 008
+	@Test
+	public void testcase_008()
+	{
+		System.out.println("Check whether the textbox outline is in red color");
+		//Enter username
+		TS_Login_001.enterUsername(NaukriInputs_UserName1);
+		
+		//Enter Password
+		TS_Login_001.enterPassword(NaukriInput_INPassWord);
+
+		//wait
+		TS_Login_001.waitMethod();
+		
+		//Click on Login Button
+		TS_Login_001.clickLoginButton();
+		
+		System.out.println("Error message should display as Please Enter Valid Email/Password");
+	}
+	
+	//Actual Test Case 009
+	@Test
+	public void testcase_009()
+	{
+		System.out.println("Check whether error message text is in red color");
+		//Enter username
+		TS_Login_001.enterUsername(NaukriInputs_UserName1);
+		
+		//Enter Password
+		TS_Login_001.enterPassword(NaukriInput_INPassWord);
+
+		//wait
+		TS_Login_001.waitMethod();
+		
+		//Click on Login Button
+		TS_Login_001.clickLoginButton();
+		
+		System.out.println("Error message should be display in red colour");
+	
+	}
+	
+	//Actual Test Case 010
+	@Test
+	public void testcase_010()
+	{
+		System.out.println("Check Wether Default Text is in Password Field");
+		System.out.println("Default Text include Should be in Password text box");
+	}
 
 	// Run After Each Test Case
 	@AfterMethod
